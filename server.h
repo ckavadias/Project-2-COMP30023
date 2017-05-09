a41f2352a38b3c4c1a66a368e6c7e87176de1d9b
ckavadias <ckavadias@users.noreply.github.com>
2017-05-09 12:07:17 +0000

    Version 2, Included thread framework for part A as well as error message padding

--- server.h
+++ server.h
@@ -6,11 +6,91 @@
 #include <sys/types.h> 
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <pthread.h>
+
+//Provided Limitations 
+#define MAX_CLIENTS 100
 
 //standard error messages
-char* pong_message = "ERRO PONG is only for the sever\r\n";
-char* okay_message = "ERRO OKAY is in fact not ok\r\n";
-char* erro_message = "ERRO ERRO this an important please dont transmit\r\n";
-char* gen_message = "ERRO Invalide protocol try again\r\n";
-char* deli_message = "ERRO delimiting incorrect\r\n";
-char* almost_ping = "ERRO just about had ping, check your spelling\r\n";
+char* pong = "ERRO PONG is only for the server";
+char* okay  = "ERRO OKAY is in fact not ok\r\n";
+char* erro = "ERRO ERRO is not a joke, please refrain";
+char* gen = "ERRO Invalide protocol try again";
+char* delim = "ERRO delimiting incorrect";
+char* almost = "ERRO just about had ping, check your spelling";
+
+//thread caller to handle socket connection
+void* receptionist(void* param){
+	int newsockfd, n = 1;
+	char buffer[256], error[40];
+	
+	newsockfd = *((int*)param);
+	
+	while(n != 0){
+	if (newsockfd < 0) 
+	{
+		perror("ERROR on accept");
+		exit(1);
+	}
+	
+	bzero(buffer,256);
+
+	/* Read characters from the connection,
+		then process */
+	
+	n = read(newsockfd,buffer,255);
+
+	if (n < 0) 
+	{
+		perror("ERROR reading from socket");
+		exit(1);
+	}
+	
+	//check which protocol is being used and respond appropriately
+	//(need to add security check for message lengths)
+	if(buffer[4] == '\r' && buffer[5] == '\n'){
+		if(buffer[0] == 'P' && buffer[2] == 'N' && buffer[3] == 'G'){
+		
+			if(buffer[1] == 'I'){
+				n = write(newsockfd, "PONG\r\n" , 6);
+			}
+			else if(buffer[1] == 'O'){
+				sprintf(error,"%s%*.*s\r\n", pong, 38-strlen(pong));
+				n = write(newsockfd, error ,strlen(error));
+			}
+			else{
+				sprintf(error,"%s%*.*s\r\n", almost, 38-strlen(almost));
+				n = write(newsockfd, error ,strlen(error));
+			}
+		}
+		else if(buffer[0] == 'O' && buffer[1] == 'K' && buffer[2] == 'A'
+			&& buffer[3] == 'Y'){
+			sprintf(error,"%s%*.*s\r\n", okay, 38-strlen(okay));
+			n = write(newsockfd, error ,strlen(error));
+		}
+		else if(buffer[0] == 'E' && buffer[1] == 'R' && buffer[2] == 'R'
+			&& buffer[3] == 'O'){
+			sprintf(error,"%s%*.*s", erro, 38-strlen(erro));
+			n = write(newsockfd, error ,strlen(error));
+		}
+		else{
+			sprintf(error,"%s%*.*s", gen, 38-strlen(gen));
+			n = write(newsockfd, error ,strlen(error));
+		}
+	}
+	else{
+		//error about delimiting 
+		sprintf(error,"%s%*.*s", delim, 38-strlen(delim));
+		n = write(newsockfd, error ,strlen(error));
+	}
+
+	
+	if (n < 0) 
+	{
+		perror("ERROR writing to socket");
+		exit(1);
+	}
+	}
+	
+	return NULL;
+}
