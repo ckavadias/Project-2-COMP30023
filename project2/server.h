afea02293b9ab9c7f6967a497855632360c01a6b
ckavadias <ckavadias@50708fc9-c562-4be5-be18-e52b07e82b13>
2017-05-10 03:46:24 +0000

    Version 3, Part A complete with padding, socket closure is still an issue but minor

--- project2/server.h
+++ project2/server.h
@@ -10,19 +10,20 @@
 
 //Provided Limitations 
 #define MAX_CLIENTS 100
+#define MAX_ERR 40
 
 //standard error messages
 char* pong = "ERRO PONG is only for the server";
-char* okay  = "ERRO OKAY is in fact not ok\r\n";
-char* erro = "ERRO ERRO is not a joke, please refrain";
-char* gen = "ERRO Invalide protocol try again";
+char* okay  = "ERRO OKAY is in fact not ok";
+char* erro = "ERRO ERRO is not a joke, please don't";
+char* gen = "ERRO Invalid protocol try again";
 char* delim = "ERRO delimiting incorrect";
-char* almost = "ERRO just about had ping, check your spelling";
+char* almost = "ERRO almost ping, check the spelling";
 
 //thread caller to handle socket connection
 void* receptionist(void* param){
-	int newsockfd, n = 1;
-	char buffer[256], error[40];
+	int newsockfd, n = 1, off = MAX_ERR - 2;
+	char buffer[256], error[MAX_ERR + 1];
 	
 	newsockfd = *((int*)param);
 	
@@ -48,40 +49,42 @@ void* receptionist(void* param){
 	
 	//check which protocol is being used and respond appropriately
 	//(need to add security check for message lengths)
-	if(buffer[4] == '\r' && buffer[5] == '\n'){
-		if(buffer[0] == 'P' && buffer[2] == 'N' && buffer[3] == 'G'){
+	if(n > 0){
+		if(buffer[4] == '\r' && buffer[5] == '\n'){
+			if(buffer[0] == 'P' && buffer[2] == 'N' && buffer[3] == 'G'){
 		
-			if(buffer[1] == 'I'){
-				n = write(newsockfd, "PONG\r\n" , 6);
+				if(buffer[1] == 'I'){
+					n = write(newsockfd, "PONG\r\n" , 6);
+				}
+				else if(buffer[1] == 'O'){
+					sprintf(error,"%s%*c\r\n", pong, off-strlen(pong),'#');
+					n = write(newsockfd, error ,MAX_ERR);
+				}
+				else{
+					sprintf(error,"%s%*c\r\n", almost, off-strlen(almost),'#');
+					n = write(newsockfd, error ,MAX_ERR);
+				}
 			}
-			else if(buffer[1] == 'O'){
-				sprintf(error,"%s%*.*s\r\n", pong, 38-strlen(pong));
-				n = write(newsockfd, error ,strlen(error));
-			}
-			else{
-				sprintf(error,"%s%*.*s\r\n", almost, 38-strlen(almost));
-				n = write(newsockfd, error ,strlen(error));
-			}
-		}
-		else if(buffer[0] == 'O' && buffer[1] == 'K' && buffer[2] == 'A'
-			&& buffer[3] == 'Y'){
-			sprintf(error,"%s%*.*s\r\n", okay, 38-strlen(okay));
-			n = write(newsockfd, error ,strlen(error));
-		}
-		else if(buffer[0] == 'E' && buffer[1] == 'R' && buffer[2] == 'R'
-			&& buffer[3] == 'O'){
-			sprintf(error,"%s%*.*s", erro, 38-strlen(erro));
-			n = write(newsockfd, error ,strlen(error));
+			else if(buffer[0] == 'O' && buffer[1] == 'K' && buffer[2] == 'A'
+				&& buffer[3] == 'Y'){
+					sprintf(error,"%s%*c\r\n", okay, off-strlen(okay),'#');
+					n = write(newsockfd, error ,MAX_ERR);
+				}
+				else if(buffer[0] == 'E' && buffer[1] == 'R' && buffer[2] == 'R'
+					&& buffer[3] == 'O'){
+					sprintf(error,"%s%*c", erro, off-strlen(erro),'#');
+					n = write(newsockfd, error ,MAX_ERR);
+					}
+					else{
+						sprintf(error,"%s%*c", gen, off-strlen(gen),'#');
+						n = write(newsockfd, error ,MAX_ERR);
+					}
 		}
 		else{
-			sprintf(error,"%s%*.*s", gen, 38-strlen(gen));
-			n = write(newsockfd, error ,strlen(error));
+			//error about delimiting 
+			sprintf(error,"%s%*c", delim, off-strlen(delim), '#');
+			n = write(newsockfd, error ,MAX_ERR);
 		}
-	}
-	else{
-		//error about delimiting 
-		sprintf(error,"%s%*.*s", delim, 38-strlen(delim));
-		n = write(newsockfd, error ,strlen(error));
 	}
 
 	
@@ -92,5 +95,6 @@ void* receptionist(void* param){
 	}
 	}
 	
+	close(newsockfd);
 	return NULL;
 }
